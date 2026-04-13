2d29b8f213a92002262bfc76cfcd373e1249123f
jojosoderqvist <glebkarpikhin@gmail.com>
2026-04-13 13:11:15 +0300

    SFB-5: added migration for creating media related tables

---
+++ migrations/20260413095505_create_media_table.down.sql
@@ -0,0 +1,3 @@
+drop table if exists "media_user_usage";
+drop table if exists "media";
+drop type if exists "media_type";
---
+++ migrations/20260413095505_create_media_table.up.sql
@@ -0,0 +1,21 @@
+create type media_type as enum ('sticker', 'gif');
+
+
+create table if not exists "media" (
+    "id" uuid not null primary key,
+    "name" text not null unique,
+    "file_id" text not null,
+    "media_type" media_type not null,
+    "added_by" bigint,
+    "created_at" timestamp with time zone not null default current_timestamp,
+    "updated_at" timestamp with time zone not null default current_timestamp
+);
+
+create table if not exists "media_user_usage" (
+    "media_id" uuid not null references "media" on (id) on delete cascade,
+    "user_id" bigint not null,
+    "usage_count" integer not null default 0,
+    "created_at" timestamp with time zone not null default current_timestamp,
+    "updated_at" timestamp with time zone not null default current_timestamp,
+    primary key (media_id, user_id)
+);
